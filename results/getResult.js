ecddd6359cc00f5074485ef7579ca715069369cd
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-01-29 09:10:34 -0800

    fix results function to pull correct ending

--- results/getResult.js
+++ results/getResult.js
@@ -4,9 +4,9 @@ export function getResult(user) {
     } else if (user.health > 40 && user.friends === 3) {
         return 2;
     } else if (user.health > 40 && user.friends < 1) {
-        return 3;
-    } else if (user.health > 40 && user.friends < 3 && user.friends > 0) {
         return 4;
+    } else if (user.health > 40 && user.friends < 3 && user.friends > 0) {
+        return 3;
     } else if (user.health <= 40 && user.health >= 25) {
         return 5;
     } else if (user.health < 25 && user.friends === 3) {
